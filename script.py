16240d5a0266197cd9335022b74e767088bec79f
lanesummer <17557428+lanesummer@users.noreply.github.com>
2023-07-11 13:36:48 -0700

    Add search alphabetically and remove none types
    Added functionality to search alphabetically; also created remove_list that removes types with no movies associated with it (ex: Z if no movie titles start with Z)

--- script.py
+++ script.py
@@ -1,21 +1,19 @@
-from textwrap import *
+import string
+from textwrap import TextWrapper, dedent
 from movie_data import *
 from linkedlist import *
 from welcome import *
 
-# add print statement for welcome here when created -----------------------
-print(welcome)
 
 # FUNCTIONS--------------------------------------------------------------------
-
 # create function to determine if user wants to search by genre, rating, etc
 def get_search_on():
     search_by = ''
     while len(search_by) == 0:
-        user_input = str(input("\nHow would you like to search for movies? Type g for genre or or r for rating.\n")).lower()
-        if user_input != 'g' and user_input != 'r':
+        user_input = str(input("\n\nHow would you like to search for movies? \nType g for genre, r for rating, a for alphabetical\n")).lower()
+        if user_input != 'g' and user_input != 'r' and user_input != 'a':
             print('\nThat is not a valid response. Please try again.')
-        else: search_by = 'genre' if user_input == 'g' else ('rating' if user_input == 'r' else print('INVALID'))
+        else: search_by = 'genre' if user_input == 'g' else ('rating' if user_input == 'r' else 'letter')
     return search_by
 
 # create code to insert genres into a data structure (LinkedList)
@@ -33,10 +31,18 @@ def insert_rating_types():
         rating_list.insert_beginning(rating)
     return rating_list
 
+# for searching alphabetically
+def insert_alphabet():
+    alphabet_list = LinkedList()
+    for letter in string.ascii_uppercase:
+        alphabet_list.insert_beginning(letter, 0)
+    return alphabet_list
+
 # create code to insert movies into a data structure (LinkedList of LinkedLists)
 # sorting by genre
 def insert_movie_data_genre():
     movie_data_list = LinkedList()
+    remove_list = []
     for genre in genres:
         genre_sublist = LinkedList()
         for movie in movies:
@@ -46,73 +52,102 @@ def insert_movie_data_genre():
                     movie_this_genre = [genre] + movie
                     genre_sublist.insert_beginning(movie_this_genre, 2)
                 index += 1
-        movie_data_list.insert_beginning(genre_sublist)
-    return movie_data_list
+        if genre_sublist.get_head_node().get_value() != None:
+            movie_data_list.insert_beginning(genre_sublist)
+        else:
+            remove_list.append(genre)
+    return movie_data_list, remove_list
 
 # sorting by movie rating
 def insert_movie_data_rating():
     movie_data_list = LinkedList()
+    remove_list = []
     for rating in movie_ratings:
         rating_sublist = LinkedList()
         for movie in movies:
             if movie[4] == rating:
                 movie_this_rating = [rating] + movie
                 rating_sublist.insert_beginning(movie_this_rating, 2)
-        movie_data_list.insert_beginning(rating_sublist)
-    return movie_data_list
+        if rating_sublist.get_head_node().get_value() != None:
+            movie_data_list.insert_beginning(rating_sublist)
+        else:
+            remove_list.append(rating)
+    return movie_data_list, remove_list
+
+# sorting by letter
+def insert_movie_data_letter():
+    movie_data_list = LinkedList()
+    remove_list = []
+    for letter in string.ascii_uppercase:
+        letter_sublist = LinkedList()
+        for movie in movies:
+            if movie[1][0] == letter:
+                movie_this_letter = [letter] + movie
+                letter_sublist.insert_beginning(movie_this_letter, 2)
+        if letter_sublist.get_head_node().get_value() != None:
+            movie_data_list.insert_beginning(letter_sublist)
+        else:
+            remove_list.append(letter)
+    return movie_data_list, remove_list
 
 
-# VARIABLES------------------------------------------------------------------------------
+# START PROGRAM HERE------------------------------------------------------------------------------
+# add print statement for welcome import from welcome.py
+print(welcome)
+
+# create lists and variables needed to run program -----------------------
+# get user input to determine how movies should be sorted/searched
 search_by = get_search_on()
 
-my_type_list = insert_genre_types() if search_by == 'genre' else insert_rating_types()
-my_movie_list = insert_movie_data_genre() if search_by == 'genre' else  insert_movie_data_rating()
+# create lists based on search parameters
+my_type_list = insert_genre_types() if search_by == 'genre' else (insert_rating_types() if search_by == 'rating' else insert_alphabet())
+my_movie_list, remove_list = insert_movie_data_genre() if search_by == 'genre' else  (insert_movie_data_rating() if search_by == 'rating' else insert_movie_data_letter())
 
 selected_type = ''
-
 
 # create code for user interaction
 while len(selected_type) == 0:
     user_input = str(input(
-        "\nWhat {0} of movie would you like to watch?\nType the beginning of that {0} and press enter to see if "
+        "\n\nWhat {0} of movie would you like to watch?\nType the beginning of that {0} and press enter to see if "
         "it's here.\n".format(search_by))).title()
 
     # Search for user_input (type) in my_type_list
     matching = []
     current_node = my_type_list.get_head_node()
     while current_node is not None:
-        if str(current_node.get_value()).title().startswith(user_input):
+        if str(current_node.get_value()).title().startswith(user_input) and not current_node.get_value() in remove_list and not current_node.get_value()[0] in remove_list:
             matching.append(current_node.get_value())
         current_node = current_node.get_next_node()
 
     # print list of matching types if more than one
     if len(matching) > 1:
-        print('\nMore than one {0} matches that search: '.format(search_by))
+        print('\n\nMore than one {0} matches that search: '.format(search_by))
         for match in matching:
             print(match)
         if user_input == 'PG':
             print("\nIf searching for PG add a space: 'PG ' \nIf searching for PG-13 add a hyphen: 'PG-'")
 
     if len(matching) == 0:
-        print('\nSorry, there are no {0}s that match that search'.format(search_by))
-
-
+        print('\n\nSorry, there are no {0}s that match that search\n'.format(search_by))
 
     # Check if only one type was found, ask user if they would like to select this type
     if len(matching) == 1:
-        select_movies = str(input('\nThere is only one {1} that matches your search: {0} \n\nDo you want to look at {0} movies? Enter y for yes and n for no.\n'.format(matching[0], search_by))).lower()
+        if search_by == 'letter':
+            select_movies = 'y'
+        else:
+            select_movies = str(input('\n\nThere is only one {1} that matches your search: {0} \n\n\nDo you want to look at {0} movies? Enter y for yes and n for no.\n'.format(matching[0], search_by))).lower()
 
         # After finding movie type, retrieve movie data for selected type
         if select_movies == 'y':
             selected_type = matching[0]
             print('\n==============================================================================')
             print('------------------------------------------------------------------------------')
-            print('Selected genre: {0}'.format(matching[0]))
+            print('Selected {1}: {0}'.format(matching[0], search_by))
             print('------------------------------------------------------------------------------\n')
             my_movie_head = my_movie_list.get_head_node()
             while my_movie_head:
                 sublist = my_movie_head.get_value().get_head_node()
-                if sublist.get_value()[0] == selected_type:
+                if sublist.get_value() is not None and sublist.get_value()[0] == selected_type:
                     while sublist is not None:
                         # create variables to make print statements more readable
                         title = sublist.get_value()[2]
@@ -130,21 +165,18 @@ while len(selected_type) == 0:
                         # print result for movie(s) of the genre selected
                         print(dedent('TITLE: {0}\n\nRated: {1}\nGenre(s): {2}\nTomatometer Score: {3}\nAudience Score: {4}\n\nOverview: {5}\n------------------------------------------------------------------------------\n'.format(title, rated, pgenre, tomato, audience, overview_wrapped)))
                         sublist = sublist.get_next_node()
-                my_movie_head = my_movie_head.get_next_node() #if my_movie_head.get_next_node()
+                my_movie_head = my_movie_head.get_next_node()
 
         # Ask user if they would like to search for other movies
-        repeat = str(input('\nWould you like to look at movies in another {0}? Enter y for yes and n for no.\n'.format(search_by))).lower()
+        repeat = str(input('\n\nWould you like to look at movies in another {0}? Enter y for yes and n for no.\n'.format(search_by))).lower()
         selected_type = ''
-        # if repeat == 'y':
-        #     pass
-        #     # selected_type = ''
         if repeat == 'n':
-            search_again = str(input('\nWould you like to start another movie search? Enter y for yes and n for no.\n'))
+            search_again = str(input('\n\nWould you like to start another movie search? Enter y for yes and n for no.\n'))
             if search_again == 'y':
                 selected_type = ''
                 search_by = get_search_on()
-                my_type_list = insert_genre_types() if search_by == 'genre' else insert_rating_types()
-                my_movie_list = insert_movie_data_genre() if search_by == 'genre' else  insert_movie_data_rating()
+                my_type_list = insert_genre_types() if search_by == 'genre' else (insert_rating_types() if search_by == 'rating' else insert_alphabet())
+                my_movie_list, remove_list = insert_movie_data_genre() if search_by == 'genre' else  (insert_movie_data_rating() if search_by == 'rating' else insert_movie_data_letter())
             else:
                 selected_type = 'end'
 print('\n\nThank you for using Summer\'s Movie Picker!\n\n')
